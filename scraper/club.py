2848506f783b730b8b72e3ed1ed65215299afbae
Ondřej Vaculík <vaculikondrej96@gmail.com>
2022-05-06 13:12:21 +0200

    df init helper, findall url function in one place

--- scraper/club.py
+++ scraper/club.py
@@ -1,14 +1,11 @@
-from tkinter.tix import Tree
 import pandas as pd
-from utils import get_soup, df_columns, df_row, this_year, today_numeric
+
+from scraper.utils import get_events, init_df, this_year, today_numeric
 
 def artbar():
-    artbar_df = pd.DataFrame(columns=df_columns, index=None)
-    row = df_row.copy()
-    row["Venue"] = "Artbar Druhý Pád"
-
-    artbar_soup = get_soup("https://www.artbar2pad.com/shows")
-    events = artbar_soup.find_all("li", {"data-hook":"events-card"})
+    artbar_df, row = init_df("Artbar Druhý Pád")
+    events = get_events("https://www.artbar2pad.com/shows",
+                        "li", {"data-hook": "events-card"})
     for event in events:
         date, month, time = event.find("div", {"data-hook": "date"}).text.split(".")
         event_datetime = pd.to_datetime(f"{this_year()}-{month}-{date} {time}")
@@ -24,17 +21,13 @@ def artbar():
 
 
 def melodka():
-    melodka_df = pd.DataFrame(columns=df_columns, index=None)
-    row = df_row.copy()
-    row["Venue"] = "Melodka"
-
+    melodka_df, row = init_df("Melodka")
     base_url = "https://www.melodka.cz/program/default/"
     _, month, year = today_numeric()
 
     for mon in range(month, month + 5):
-        melodka_soup = get_soup(f"{base_url}{year}-{mon}")
-        events = melodka_soup.find_all("div", {"class": "program_line2"})
-
+        events = get_events(f"{base_url}{year}-{mon}",
+                             "div", {"class": "program_line2"})
         for event in events:
             date = event.find("div", {"class": "datum"}).text
             # print(date)
@@ -48,12 +41,9 @@ def melodka():
     return melodka_df
 
 def metro():
-    metro_df = pd.DataFrame(columns=df_columns)
-    row = df_row.copy()
-    row["Venue"] = "Metro Music"
-
-    metro_soup = get_soup("https://www.metromusic.cz/program/")
-    events = metro_soup.find_all("div", {"class": "item-inner"})
+    metro_df, row = init_df("Metro Music")  
+    events = get_events("https://www.metromusic.cz/program/", "div",
+                          {"class": "item-inner"})
     for event in events:
         date, time = event.find("p", {"class": "date"}).text.strip().split(" ")
         event_datetime = pd.to_datetime(f"{date}/{this_year()}/{time[1:-1]}",
@@ -66,14 +56,11 @@ def metro():
         if info:
             row["Info"] = info.text.strip()
         metro_df.loc[len(metro_df.index)] = row
-        
+
     return metro_df
 
-def main():
+def clubs():
+    """Scrape all clubs"""
     df_all = pd.concat([artbar(), melodka(), metro()])
     df_all = df_all.sort_values(by="Date")[["Date", "Name", "Venue", "Link"]]
-    df_all.to_csv("out/all.csv", index=False)
-
-if __name__ == "__main__":
-    # print(metro())
-    main()
+    return df_all
--- scraper/utils.py
+++ scraper/utils.py
@@ -1,19 +1,32 @@
 import json
 from datetime import datetime
 
+import pandas as pd
 from requests import get, post
 from bs4 import BeautifulSoup
 
 df_columns = ["Date", "Time", "Name", "Venue", "Price", "Link", "Info"] # doplnit z Dell
 df_row = {column:None for column in df_columns}
 
-def get_soup(url: str):
+def init_df(venue_name):
+    df = pd.DataFrame(columns=df_columns, index=None)
+    row = df_row.copy()
+    row["Venue"] = venue_name
+    return df, row
+
+def get_soup(url: str) -> BeautifulSoup:
     response = get(url)
     if response.status_code == 200:
         print(f"OK - {url}")
+        return BeautifulSoup(response.text, 'html.parser')
     else:
         print(f"ERROR - {url}")
-    return BeautifulSoup(response.text, 'html.parser')
+    
+
+def get_events(url: str, tag: str, attrs: dict):
+    soup = get_soup(url)
+    if soup:
+        return soup.find_all(tag, attrs)  
 
 def post_soup(url: str, payload):
     response = post(url, data=payload)
