e698e8760789e167d93da9e8e9eba990c6c81548
Ondřej Vaculík <vaculikondrej96@gmail.com>
2022-10-01 14:02:06 +0200

    Kjogen

--- scraper/theatre.py
+++ scraper/theatre.py
@@ -8,9 +8,9 @@ from scraper.utils import get_json, get_events, init_df, this_year, this_month
 def colabs():
     colabs_df, row = init_df("CO.LABS")
     urls = [
-        "https://colabs.cz/program/?categoryIds%5B0%5D=1",
-        "https://colabs.cz/program/?categoryIds%5B0%5D=3",
-        "https://colabs.cz/program/?categoryIds%5B0%5D=2"
+        "http://www.colabs.cz/program/?categoryIds%5B0%5D=1",
+        # "https://colabs.cz/program/?categoryIds%5B0%5D=3",
+        # "https://colabs.cz/program/?categoryIds%5B0%5D=2"
     ]
     for url in urls:
         events = get_events(url, "div", {"class": "event-details"})
@@ -26,12 +26,13 @@ def colabs():
             title = event.find("div", {"class": "event-title"})
             row["Name"] = title.text.strip()
             row["Link"] = "http://www.colabs.cz" + title.a["href"]
-            category_price = event.find("div", {"class": "event-category-price"})
-            if category_price:
-                _, category, price = category_price.text.split("//")
-                row["Info"] = category.strip()
-                if price:
-                    row["Price"] = price.replace(",-", "").strip()
+            category = event.find("div", {"class": "event-category-price"})
+            if category:
+                category_text = category.text.split("//")[1].strip()
+                row["Info"] = category_text.strip()
+            price = event.find("div", {"class": "event-buy-tickets"})
+            if price:
+                row["Price"] = price.text.split(" ")[2]
             colabs_df.loc[len(colabs_df.index)] = row
 
     return colabs_df
@@ -44,14 +45,25 @@ def kjogen():
         # print(url)
         table = get_events(url, "table", {"class": "program"})
         if table:
-            for row in table[0].find("tbody").find_all("tr"):
-                date_time, venue, program = row.find_all("td")
-                print(date_time.text, venue.text, program, sep="\n", end="\n\n")
+            for trow in table[0].find("tbody").find_all("tr"):
+                date_time, venue_info, program = trow.find_all("td")
+                day, date, time = date_time.get_text(strip=True, separator='\n').splitlines()
+                if "Brno" in venue_info.text:
+                    city, row["Venue"], address = venue_info.get_text(strip=True, separator='\n').splitlines()
+                    row["Name"]= program.get_text(strip=True, separator=', ').replace(", VSTUPENKY", "")
+                    event_datetime = pd.to_datetime(date + time, format="%d.%m.%Y%H:%M")
+                    row["Date"] = event_datetime.date()
+                    row["Time"] = event_datetime.time()
+                    if program.find("a"):
+                        row["Link"] = program.find("a")["href"]
+                    kjogen_df.loc[len(kjogen_df.index)] = row
+    
+    return kjogen_df
 
 def theatres():
-    # df_theatres = pd.concat([colabs()])
-    df_theatres = colabs()
-    df_theatres = df_theatres.sort_values(by="Date")[["Date", "Name", "Venue", "Link"]]
+    df_theatres = pd.concat([colabs(), kjogen()])
+    # df_theatres = colabs()
+    df_theatres = df_theatres.sort_values(by="Date")
     return df_theatres
 
 
